11dca35aae2abf3230f978dc5ed9846be02c0c04
Sharkie <craig.sharkie@gmail.com>
2023-02-23 20:41:46 +1100

    Streamlining `path`

--- scripts/apothecary.js
+++ scripts/apothecary.js
@@ -1,17 +1,12 @@
 window.addEventListener('load', (event) => {
   const filter = "PTHCRY| ";
-  const path = window.location.pathname;
+  const pathname = window.location.pathname;
+  const path = pathname.substr(1);
   const slug = path.split("/");
 
   slug.shift();
 
-  /*
-    if (slug[0] === "range" || slug[0] === "store") {
-      document.body.classList.add("path-" + slug[0]);
-    }
-  */
-
-  document.body.classList.add("path-" + slug[0]);
+  document.body.classList.add("path-" + path + ", slug-" + slug[0]);
 
   const gallery = document.querySelector("a[href='/range/gallery']");
   if (gallery) {
