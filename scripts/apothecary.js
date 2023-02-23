910126562dbe2012a9b0364e7d5b18e861559fed
Sharkie <craig.sharkie@gmail.com>
2023-02-23 19:39:59 +1100

    Checking `path`

--- scripts/apothecary.js
+++ scripts/apothecary.js
@@ -3,8 +3,12 @@ window.addEventListener('load', (event) => {
   const slug = path.split("/");
   slug.shift();
 
+  console.log(path)
+
   if (slug[0] === "range") {
     document.body.classList.add("slug-" + slug[1]);
+  } else if (path === "/store") {
+    document.body.classList.add("path-store");
   }
 
   const gallery = document.querySelector("a[href='/range/gallery']");
